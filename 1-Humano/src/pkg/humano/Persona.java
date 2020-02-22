ca9faa5968004aebb2e466033c89956c8b1a5c94
Estuardo-Sazo <estuardosazo1206@gmail.com>
2020-02-22 11:07:49 -0600

    Update Persona.java

--- 1-Humano/src/pkg/humano/Persona.java
+++ 1-Humano/src/pkg/humano/Persona.java
@@ -17,7 +17,7 @@ public class Persona {
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
-    }
+    }    
 
     public String getNombre() {
         return nombre;
